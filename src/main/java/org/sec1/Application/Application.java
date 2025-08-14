5523d5a539998e4897633c04bd62fef8bf64a5cd
JHPC003\hnjh1 <hnjh12195@naver.com>
2025-08-14 15:46:30 +0900

    alpha

--- src/main/java/org/sec1/Application/Application.java
+++ src/main/java/org/sec1/Application/Application.java
@@ -3,5 +3,6 @@ package org.sec1.Application;
 public class Application {
     public static void main(String[] args) {
         System.out.println("test1");
+        System.out.println("this is alpha branch");
     }
 }
