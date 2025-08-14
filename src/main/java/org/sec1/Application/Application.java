bfc16c9aa170e1dd7df7d6f4b603d05582d0f7df
JHPC003\hnjh1 <hnjh12195@naver.com>
2025-08-14 15:47:25 +0900

    main branch

--- src/main/java/org/sec1/Application/Application.java
+++ src/main/java/org/sec1/Application/Application.java
@@ -2,6 +2,7 @@ package org.sec1.Application;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("test1");
+        System.out.println("this is main branch ");
+
     }
 }
